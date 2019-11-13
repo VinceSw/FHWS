c626f82aeaa3ed272fbbec0b8dea0d535959ddf7
VinceSw <VinceSw@users.noreply.github.com>
2019-11-13 20:13:12 +0100

    Test

---
+++ Prog1_FirstApplication/src/de/fhws/fiw/Tausch/Tausch.java
@@ -0,0 +1,19 @@
+package de.fhws.fiw.Tausch;
+
+public class Tausch {
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 7;
+//Vor dem Tausch
+		System.out.println("Wert von x vor dem Tausch: " + x);
+		System.out.println("Wert von y vor dem Tausch: " + y + "\n");
+//Tausch
+		//Definition einer Hilfsvariable als Zwischenspeicher für den Tausch
+		int zwischenvariable = x;
+		x = y;
+		y = zwischenvariable;
+//Nach dem Tausch
+		System.out.println("Wert von x nach dem Tausch: " + x);
+		System.out.println("Wert von y nach dem Tausch: " + y);
+	}
+}
