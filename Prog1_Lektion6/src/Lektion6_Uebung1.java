41e42a5cf5ec233f74464a726af3c24c3d337938
VinceSw <VinceSw@users.noreply.github.com>
2019-11-17 23:13:05 +0100

    Lektion 6 Übung 1 --- Teil A fertig (Rundungsmethode) - Teil B noch in Arbeit (Bestimmte Anzahl an Nachkommastellen runden)

--- Prog1_Lektion6/src/Lektion6_Uebung1.java
+++ Prog1_Lektion6/src/Lektion6_Uebung1.java
@@ -1,11 +1,61 @@
+import java.util.Scanner;
 
 public class Lektion6_Uebung1
 {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		Scanner inputScan = new Scanner(System.in);
+		System.out.print("Bitte geben Sie eine Fließkommazahl ein: ");
+		double inputZahl = Double.valueOf(inputScan.nextLine());
+		
+		System.out.print("\nBitte geben Sie die gewünschte Genauigkeit der Rundung ein: ");
+		if(inputScan.hasNextInt())
+		{
+			int precision = Integer.valueOf(inputScan.nextLine());
+	
+			if(precision > 0)
+			{
+				System.out.println(customRound(inputZahl, precision));
+			}
+		}
+		else
+		{
+			System.out.println(customRound(inputZahl));
+		}
+		
+		inputScan.close();
+	}
+	
+	public static double customRound(double a)
+	{
+		int testVar = (int)a;
+		
+		if(a - testVar >= 0.5)
+		{
+			testVar++; 
+		}
+		
+		return testVar;
+	}
+	
+	public static double customRound(double a, int precision)
+	{
+		double exponent = getExponent(precision);
+		
+		return exponent;
+	}
+	
+	public static double getExponent(int precision)
+	{
+		double exponent = 1;
+		
+		for(int i = 0; i < precision; i++)
+		{
+			exponent /= 10;
+		}
+		
+		return exponent;
 	}
 
 }
