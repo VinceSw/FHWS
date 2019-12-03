30ec5384a4bc221d817b45d93314cdc691d179a1
VinceSw <VinceSw@users.noreply.github.com>
2019-12-03 16:34:04 +0100

    CaesarList_Rekursiv

--- AlgoDat_I/Übung7/Aufgabe37_caesarList.rkt
+++ AlgoDat_I/Übung7/Aufgabe37_caesarList.rkt
@@ -1,5 +1,6 @@
 #lang racket
-(define (caesar_encrypt_list data key)
+;Iterativ
+(define (caesar_encrypt_list_i data key)
   (caesar-iter data key key (list)))
 
 (define (caesar-iter data key keyTemp newList)
@@ -9,9 +10,7 @@
           ))
       (if (null? keyTemp)
           (caesar-iter data key key newList)
-          (caesar-iter (cdr data) key (cdr keyTemp) (cons (if (>= (+ (car data) (car keyTemp)) 10)
-                                                              (- (+ (car data) (car keyTemp)) 10)
-                                                              (+ (car data) (car keyTemp))) newList))
+          (caesar-iter (cdr data) key (cdr keyTemp) (cons (remainder (+ (car data) (car keyTemp)) 10) newList))
           )
       )
   )
@@ -23,8 +22,24 @@
       )
   )
 
+
+;Rekursiv
+(define nil (list))
+
+(define (caesar_encrypt_list data key)
+  (define (iter key2 data2)
+    (if  (null? data2)
+             nil
+             (if (null?  key2)
+                 (iter key data2)
+                 (cons (remainder (+ (car data2) (car key2)) 10) (iter (cdr key2) (cdr data2)))
+             )
+        )
+    )
+  (iter key data)
+  )
 ; ============================= Beispiele =============================
 
-(caesar_encrypt_list (list 1 2 3 4 5 6) (list 1 3 3 7))
+(caesar_encrypt_list (list 1 2 3 4 5 6) (list 1 3 3 77))
 (caesar_encrypt_list (list 0 3 0 1 1 9 8 9) (list 2 0 1 4))
 (caesar_encrypt_list (list 9 0 0 7 1 1 9 2) (list 1 9 9 9))
