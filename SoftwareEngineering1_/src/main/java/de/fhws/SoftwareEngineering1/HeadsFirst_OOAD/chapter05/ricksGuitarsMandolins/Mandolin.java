a304623e8c07685742e20d9955b41a855decc7ab
VinceSw <VinceSw@users.noreply.github.com>
2020-05-06 21:07:00 +0200

    SoftwareEngineering 1 initialer Load

--- SoftwareEngineering1_/src/main/java/de/fhws/SoftwareEngineering1/HeadsFirst_OOAD/chapter05/ricksGuitarsMandolins/Mandolin.java
+++
@@ -1,7 +0,0 @@
-package de.fhws.SoftwareEngineering1.HeadsFirst_OOAD.chapter05.ricksGuitarsMandolins;
-public class Mandolin extends Instrument {
- 
-  public Mandolin(String serialNumber, double price, MandolinSpec spec) {
-    super(serialNumber, price, spec);
-  }
-}
--- SoftwareEngineering1_/src/main/java/de/fhws/SoftwareEngineering1/HeadsFirst_OOAD/chapter07/gsf/unit/Unit.java
+++
@@ -1,9 +0,0 @@
-package de.fhws.SoftwareEngineering1.HeadsFirst_OOAD.chapter07.gsf.unit;
-
-public class Unit
-{
-    
-    public Unit()
-    {
-    }
-}
